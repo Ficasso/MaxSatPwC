1dc37ea82c312b11b0fe0a254f731946ccfa2996
Ficasso <pawel.filiciak@gmail.com>
2020-11-12 10:37:17 +0100

    max sat implementation

--- src/main/java/com/sp/maxsat/satisfaction/MaxSat.java
+++ src/main/java/com/sp/maxsat/satisfaction/MaxSat.java
@@ -9,7 +9,7 @@ public class MaxSat {
     private boolean maxSat(Map<Integer, Formula> clauses, List<Integer> clausesToCheck, LogicalEnum logicalEnum) {
         var checked = new ArrayList<Boolean>();
         for (int i = 0; i < clausesToCheck.size(); i++) {
-            var clause = clauses.get(i);
+            var clause = clauses.get(clausesToCheck.get(i));
             if (logicalEnum == LogicalEnum.LESS) {
                 checked.add(clause.getFirst() < clause.getSecond() && clause.getSecond() < clause.getThird());
             } else if (logicalEnum == LogicalEnum.EQUAL) {
